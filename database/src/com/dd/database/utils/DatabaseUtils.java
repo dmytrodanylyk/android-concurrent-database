4bb2076b1a5f6ce42697b8c0958bf762c12bb05b
Dmytro Danylyk <dmytrodanylyk@gmail.com>
2014-02-20 14:26:32 +0200

    Update DatabaseUtils.java

--- database/src/com/dd/database/utils/DatabaseUtils.java
+++ database/src/com/dd/database/utils/DatabaseUtils.java
@@ -5,24 +5,24 @@ import android.database.sqlite.SQLiteDatabase;
 
 public class DatabaseUtils {
 
-    public int delete(SQLiteDatabase database, DatabaseParams.Delete params) {
+    public static int delete(SQLiteDatabase database, DatabaseParams.Delete params) {
         return database.delete(params.table, params.whereClause, params.whereArgs);
     }
 
-    public long insert(SQLiteDatabase database, DatabaseParams.Insert params) {
+    public static long insert(SQLiteDatabase database, DatabaseParams.Insert params) {
         return database.insert(params.table, params.nullColumnHack, params.values);
     }
 
-    public Cursor select(SQLiteDatabase database, DatabaseParams.Select params) {
+    public static Cursor select(SQLiteDatabase database, DatabaseParams.Select params) {
         return database.query(params.distinct, params.table, params.columns, params.selection,
                 params.selectionArgs, params.groupBy, params.having, params.orderBy, params.limit);
     }
 
-    public int update(SQLiteDatabase database, DatabaseParams.Update params) {
+    public static int update(SQLiteDatabase database, DatabaseParams.Update params) {
         return database.update(params.table, params.values, params.whereClause, params.whereArgs);
     }
 
-    public Cursor rawQuery(SQLiteDatabase database, String sql, String[] selectionArgs) {
+    public static Cursor rawQuery(SQLiteDatabase database, String sql, String[] selectionArgs) {
         return database.rawQuery(sql, selectionArgs);
     }
 }

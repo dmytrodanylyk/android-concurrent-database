10dd83dcc13d8f585ee4c4fb084d60728da36e05
Dmytro Danylyk <dmytrodanylyk@gmail.com>
2014-10-08 11:46:26 +0300

    Update ArrayUtils.java

--- database-test/src/com/test/database/utils/ArrayUtils.java
+++ database-test/src/com/test/database/utils/ArrayUtils.java
@@ -3,10 +3,15 @@ package com.test.database.utils;
 public class ArrayUtils {
 
     public static String[] build(Object... values) {
-        String[] arr = new String[values.length];
-        for (int i = 0; i < values.length; i++) {
-            arr[i] = String.valueOf(values[i]);
-        }
-        return arr;
-    }
+		String[] arr = new String[values.length];
+		for (int i = 0; i < values.length; i++) {
+			if (values[i] instanceof Boolean) {
+				boolean value = (boolean) values[i];
+				arr[i] = String.valueOf(value ? 1 : 0);
+			} else {
+				arr[i] = String.valueOf(values[i]);
+			}
+		}
+		return arr;
+	}
 }
